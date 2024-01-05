391570eb3fdca0d04689c858b1d92f301087faef
Yann Christophe <77779939+Yann0411@users.noreply.github.com>
2024-01-05 15:22:27 +0100

    event intro

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+
+var playerElm = document.getElementById('player')
+var dealerElm = document.getElementById('dealer')
+
+
 function getRandomNumber(min = 1, max = 6) {
     min = Math.ceil(min);
     max = Math.floor(max);
@@ -18,25 +23,54 @@ function createDice() {
     var backgroundPositionXDice = numberToDicePosition(diceNumber)
 
     diceElm.style.backgroundPositionX = `${backgroundPositionXDice}px`
+    diceElm.addEventListener('click',makeDiceBeautiful)
 
     return diceElm;
 }
 
+function makeDiceBeautiful(event){
 
-
-var nbDices = Number(prompt('Nombre de dés'))
-var playerElm = document.getElementById('player')
-var dealerElm = document.getElementById('dealer')
-
-for (let nbDice = 0; nbDice < nbDices; nbDice++) {
-
-    var dicePlayerElm = createDice()
-    playerElm.append(dicePlayerElm)
-    var diceDealerElm = createDice()
-    dealerElm.append(diceDealerElm)
-
+    var diceElm= event.target
+    console.log(event)
+    diceElm.style.border ='2px solid #F0F'
 }
 
+function createAndAddDiceToDOM(gameZone) {
+    var diceElm = createDice()
+    gameZone.append(diceElm)
+}
+
+
+function reset() {
+    playerElm.textContent = '';
+    dealerElm.textContent = '';
+}
+
+
+function play(event) {
+
+
+
+    console.log(event)
+    console.log(event.target)
+
+    var nbDices=6
+
+    reset()
+
+
+    for (let nbDice = 0; nbDice < nbDices; nbDice++) {
+
+        createAndAddDiceToDOM(playerElm)
+        createAndAddDiceToDOM(dealerElm)
+
+
+    }
+}
+
+
+var playBtnElm = document.querySelector('#play')
+playBtnElm.addEventListener('click',play)
 
 
 
