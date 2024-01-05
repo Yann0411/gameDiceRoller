8cc34c5f6d7c9d03f3d011279cd30d162425514d
Yann Christophe <77779939+Yann0411@users.noreply.github.com>
2024-01-05 11:52:25 +0100

    amelioration du code

--- js/app.js
+++ js/app.js
@@ -1,63 +1,44 @@
-console.log('toto')
-
-
-
-
-function createDice1() {
-    var divElm = document.createElement('div')
-    divElm.classList.add('dice')
-
-    var playerId = document.querySelector('#player')
-    playerId.appendChild(divElm)
-    var random = getRandomIntInclusive(6)
-
-
-
-    newPosition = (random - 1) * -100
-
-    divElm.style.backgroundPosition = `${newPosition}px`
-
-}
-
-function createDice2() {
-    var divElm = document.createElement('div')
-    divElm.classList.add('dice')
-
-    var dealerId = document.querySelector('#dealer')
-    dealerId.appendChild(divElm)
-    var random = getRandomIntInclusive(6)
-
-    
-
-    newPosition = (random - 1) * -100
-
-    divElm.style.backgroundPosition = `${newPosition}px`
-
-}
-
-
-
-
-
-
-function getRandomIntInclusive(max, min = 1) {
+function getRandomNumber(min = 1, max = 6) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-
-function askUser(){
-    var user = Number(prompt('Combien de dé veux-tu lancer ?'))
-    for (let index = 0; index < user; index++) {
-        createDice1()* index
-        createDice2()*index
-        
-    }
-   
+function numberToDicePosition(number) {
+    return (number - 1) * 100 * -1
 }
 
-askUser()
+
+function createDice() {
+
+    var diceElm = document.createElement('div')
+
+    diceElm.classList.add('dice')
+    var diceNumber = getRandomNumber()
+    var backgroundPositionXDice = numberToDicePosition(diceNumber)
+
+    diceElm.style.backgroundPositionX = `${backgroundPositionXDice}px`
+
+    return diceElm;
+}
+
+
+
+var nbDices = Number(prompt('Nombre de dés'))
+var playerElm = document.getElementById('player')
+var dealerElm = document.getElementById('dealer')
+
+for (let nbDice = 0; nbDice < nbDices; nbDice++) {
+
+    var dicePlayerElm = createDice()
+    playerElm.append(dicePlayerElm)
+    var diceDealerElm = createDice()
+    dealerElm.append(diceDealerElm)
+
+}
+
+
+
 
 
 
